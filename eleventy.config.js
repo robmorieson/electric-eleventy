7dddf9b62c0d1f836c092655aa0e96cb362984ca
Rob Morieson <rob@electricanimals.com>
2024-05-24 00:43:01 +1000

    Add lightening css and basic styling

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,7 @@
+import lightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
+
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(lightningCSS);
   return {
     dir: {
       input: "src",
