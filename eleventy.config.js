9114e1ded6ee25774f3bc745be59fbd87a67916d
Rob Morieson <rob@electricanimals.com>
2024-05-30 16:14:10 +1000

    Styling, copy and favicon :nail_care: :memo: :zap:

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ import lightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(lightningCSS);
+  eleventyConfig.addPassthroughCopy("src/assets/images");
   return {
     dir: {
       input: "src",
